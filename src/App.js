4bfcb801d5d405f39c27205f10ee9ca4ecbe2137
Guy Sanchez <guy.sanchez@nic.uni.edu.ni>
2021-03-16 16:08:12 -0600

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import * as XLSX from 'xlsx';
+import { useState } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const styles = makeStyles({
+  table: {
+    minWidth: 700,  
+  }
+})
 
 function App() {
+  const[file, setFile] = useState([])
+  const classes = styles();
+
+  const readExcel=(file) =>{
+    const promise = new Promise((resolve, reject)=>{
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload=(e)=>{
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, {type:'array'});
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        const data = XLSX.utils.sheet_to_json(ws, {header:1});
+        resolve(data);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+    promise.then((d) =>{
+      console.log(d)
+      setFile(d)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+         <div className="row justify-content-md-center">
+          <input className="col-md-auto int" type="file" onChange={(e) =>{
+            const file = e.target.files[0]
+            readExcel(file)
+          }} />
+         </div>
+       </div>
+
+        <TableContainer className="container" component={Paper}>
+          <Table className={classes.table, "row"} aria-label="simple table">
+          {file.map((d) =>(
+              <TableHead key={d.name}>
+                <TableBody>
+                  <TableRow className="col col-md-4">
+                    <TableCell className="col" scope="row">
+                      {d} 
+                    </TableCell>
+                  </TableRow>
+              </TableBody>
+              </TableHead>
+          ))}
+          </Table>
+        </TableContainer>
+        
     </div>
   );
 }
